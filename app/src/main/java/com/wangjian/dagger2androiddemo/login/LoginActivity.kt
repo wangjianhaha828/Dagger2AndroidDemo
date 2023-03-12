e264e5545566020629f47c3dbeea7f8a61be7596
wangjian <1637299051@qq.com>
2023-03-12 14:39:56 +0800

    界面创建

---
+++ app/src/main/java/com/wangjian/dagger2androiddemo/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.wangjian.dagger2androiddemo.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wangjian.dagger2androiddemo.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
